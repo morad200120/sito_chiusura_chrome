4bf06ca4d2956914466826b84ebfd1161db3547e
Mourad <morad@x4brs.onmicrosoft.com>
2025-01-25 02:40:23 +0100

    Refactor logging setup in Flask application for enhanced organization and clarity

--- flask_app.py
+++ flask_app.py
@@ -11,168 +11,161 @@ import user_agents
 import subprocess
 
 # ----------------------------------------------------------------------------------------------------
-
+# Caricamento delle variabili d'ambiente
 load_dotenv()
 
-def start_site(port):
+# ----------------------------------------------------------------------------------------------------
+# Configurazione dell'app Flask
+app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
-    app = Flask(__name__)
-    app.secret_key = os.getenv("FLASK_SECRET_KEY")
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SECURE=True,
+    SESSION_PERMANENT=False,
+    PERMANENT_SESSION_LIFETIME=timedelta(minutes=30))
 
-    app.config.update(
-        SESSION_COOKIE_HTTPONLY=True,
-        SESSION_COOKIE_SECURE=True,
-        SESSION_PERMANENT=False,
-        PERMANENT_SESSION_LIFETIME=timedelta(minutes=30))
-
-    # ----------------------------------------------------------------------------------------------------
-
+# ----------------------------------------------------------------------------------------------------
+# Configurazione dei logger
+def setup_logger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    debug_handler = logging.FileHandler("debug.log", encoding='utf-8')
-    debug_handler.setLevel(logging.DEBUG)
-    debug_handler.addFilter(lambda record: record.levelno == logging.DEBUG)
-
-    info_handler = logging.FileHandler("info.log", encoding='utf-8')
-    info_handler.setLevel(logging.INFO)
-    info_handler.addFilter(lambda record: record.levelno == logging.INFO)
-
-    warning_handler = logging.FileHandler("warning.log", encoding='utf-8')
-    warning_handler.setLevel(logging.WARNING)
-    warning_handler.addFilter(lambda record: record.levelno == logging.WARNING)
-
-    error_handler = logging.FileHandler("error.log", encoding='utf-8')
-    error_handler.setLevel(logging.ERROR)
-    error_handler.addFilter(lambda record: record.levelno == logging.ERROR)
-
-    critical_handler = logging.FileHandler("critical.log", encoding='utf-8')
-    critical_handler.setLevel(logging.CRITICAL)
-    critical_handler.addFilter(lambda record: record.levelno == logging.CRITICAL)
-
+    # Configurazione degli handler per diversi livelli di log
     log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    
-    debug_handler.setFormatter(log_format)
-    info_handler.setFormatter(log_format)
-    warning_handler.setFormatter(log_format)
-    error_handler.setFormatter(log_format)
-    critical_handler.setFormatter(log_format)
 
-    logger.addHandler(debug_handler)
-    logger.addHandler(info_handler)
-    logger.addHandler(warning_handler)
-    logger.addHandler(error_handler)
-    logger.addHandler(critical_handler)
+    # Handler per i vari livelli di log
+    handlers = {
+        logging.DEBUG: logging.FileHandler("debug.log", encoding='utf-8'),
+        logging.INFO: logging.FileHandler("info.log", encoding='utf-8'),
+        logging.WARNING: logging.FileHandler("warning.log", encoding='utf-8'),
+        logging.ERROR: logging.FileHandler("error.log", encoding='utf-8'),
+        logging.CRITICAL: logging.FileHandler("critical.log", encoding='utf-8')
+    }
 
-    logger.debug("Questo è un messaggio DEBUG")
-    logger.info("Questo è un messaggio INFO")
-    logger.warning("Questo è un messaggio WARNING")
-    logger.error("Questo è un messaggio ERROR")
-    logger.critical("Questo è un messaggio CRITICAL")
+    # Aggiunta dei filtri per ogni livello
+    for level, handler in handlers.items():
+        handler.setLevel(level)
+        handler.setFormatter(log_format)
+        logger.addHandler(handler)
 
-    # ----------------------------------------------------------------------------------------------------
+    return logger
 
-    USERNAME = os.getenv('USERNAME')
-    PASSWORD = os.getenv('PASSWORD')
+logger = setup_logger()
 
-    # ----------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
+# Funzione per ottenere l'User-Agent e redirigere in base al tipo di dispositivo
+def ottieni_ua(pc_route, mobile_route):
+    user_agent = request.headers.get('User-Agent')
+    ua = user_agents.parse(user_agent)
+    return redirect(url_for(pc_route if ua.is_pc else mobile_route))
 
-    def ottieni_ua(pc_route, mobile_route):
-        user_agent = request.headers.get('User-Agent')
-        ua = user_agents.parse(user_agent)
-        return redirect(url_for(pc_route if ua.is_pc else mobile_route))
+# ----------------------------------------------------------------------------------------------------
+# Definizione delle variabili di login
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
 
-    # ----------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
+# Gestione dei tentativi falliti di login
+failed_attempts = {}
+BLOCK_TIME = timedelta(minutes=15)
 
-    @app.route("/", methods=["GET"])
-    def first_redirect():
-        logger.info("Richiesta ricevuta: Prima redirezione")
-        log_request_data()
-        return ottieni_ua("login_desktop", "login_mobile")
+# ----------------------------------------------------------------------------------------------------
+# Funzione per loggare i dettagli della richiesta
+def log_request_data():
+    user_agent = request.headers.get('User-Agent')
+    ua = user_agents.parse(user_agent)
+    device = ua.device.family
+    browser = ua.browser.family
+    model = ua.device.model if ua.device.model else "Unknown"
+    ip_address = request.remote_addr
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Data e ora
 
-    # ----------------------------------------------------------------------------------------------------
+    logger.info(f"Data/Ora: {timestamp} | IP: {ip_address} | Dispositivo: {device} | "
+                f"Browser: {browser} | Modello: {model}")
 
-    failed_attempts = {}
-    BLOCK_TIME = timedelta(minutes=15)
+# ----------------------------------------------------------------------------------------------------
+# Route per la prima redirezione
+@app.route("/", methods=["GET"])
+def first_redirect():
+    logger.info("Richiesta ricevuta: Prima redirezione")
+    log_request_data()
+    return ottieni_ua("login_desktop", "login_mobile")
 
-    @app.route("/login", methods=["POST", "GET"])
-    def login():
-        username = request.form.get("username")
-        password = request.form.get("password")
-        ip_address = request.remote_addr
-        current_time = datetime.now()
+# ----------------------------------------------------------------------------------------------------
+# Route per il login
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    ip_address = request.remote_addr
+    current_time = datetime.now()
 
-        if request.method == 'GET':
-            return render_template("login_desktop.html")
-
-        if username == USERNAME and password == PASSWORD:
-            failed_attempts[ip_address] = {'attempts': 0, 'last_attempt': current_time}
-            log_request_data()
-            logger.info(f"Login riuscito per {username} da {ip_address}")
-            return ottieni_ua("desktop", "mobile")
-        else:
-            failed_attempts[ip_address] = {
-                'attempts': failed_attempts.get(ip_address, {}).get('attempts', 0) + 1,
-                'last_attempt': current_time
-            }
-            flash("Username o password errati", "error")
-            log_request_data()
-            logger.error(f"Login fallito per {username} da {ip_address}")
-            return redirect(url_for("login"))
-
-    # ----------------------------------------------------------------------------------------------------
-
-    def log_request_data():
-        user_agent = request.headers.get('User-Agent')
-        ua = user_agents.parse(user_agent)
-        device = ua.device.family
-        browser = ua.browser.family  
-        model = ua.device.model if ua.device.model else "Unknown"
-        ip_address = request.remote_addr 
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-        logger.info(f"Data/Ora: {timestamp} | IP: {ip_address} | Dispositivo: {device} | "
-                    f"Browser: {browser} | Modello: {model}")
-
-    # ----------------------------------------------------------------------------------------------------
-
-    @app.route("/spegnimento", methods=["POST"])
-    def spegnimento():
-        try:
-            seconds = 90
-            subprocess.run(f"shutdown /s /t {seconds}", shell=True, check=True)
-            logger.info(f"Il computer si spegnerà tra {seconds} secondi.")
-            flash("Il computer si spegnerà a breve.", "success")
-            return ottieni_ua("desktop", "mobile")
-        except Exception as e:
-            logger.error(f"Errore durante lo spegnimento: {str(e)}")
-            flash(f"Errore durante lo spegnimento: {str(e)}", "error")
-            return ottieni_ua("desktop", "mobile")
-
-    # ----------------------------------------------------------------------------------------------------
-
-    @app.route("/login_desktop", methods=["GET"])
-    def login_desktop():
+    if request.method == 'GET':
         return render_template("login_desktop.html")
 
-    @app.route("/login_mobile", methods=["GET"])
-    def login_mobile():
-        return render_template("login_mobile.html")
-    
-    # ----------------------------------------------------------------------------------------------------
+    # Verifica delle credenziali
+    if username == USERNAME and password == PASSWORD:
+        failed_attempts[ip_address] = {'attempts': 0, 'last_attempt': current_time}
+        log_request_data()
+        logger.info(f"Login riuscito per {username} da {ip_address}")
+        return ottieni_ua("desktop", "mobile")
+    else:
+        failed_attempts[ip_address] = {
+            'attempts': failed_attempts.get(ip_address, {}).get('attempts', 0) + 1,
+            'last_attempt': current_time
+        }
+        flash("Username o password errati", "error")
+        log_request_data()
+        logger.error(f"Login fallito per {username} da {ip_address}")
+        return redirect(url_for("login"))
 
-    @app.route("/desktop")
-    def desktop():
-        return render_template("desktop.html")
+# ----------------------------------------------------------------------------------------------------
+# Route per il comando di spegnimento del computer
+@app.route("/spegnimento", methods=["POST"])
+def spegnimento():
+    try:
+        seconds = 90
+        subprocess.run(f"shutdown /s /t {seconds}", shell=True, check=True)
+        logger.info(f"Il computer si spegnerà tra {seconds} secondi.")
+        flash("Il computer si spegnerà a breve.", "success")
+        return ottieni_ua("desktop", "mobile")
+    except Exception as e:
+        logger.error(f"Errore durante lo spegnimento: {str(e)}")
+        flash(f"Errore durante lo spegnimento: {str(e)}", "error")
+        return ottieni_ua("desktop", "mobile")
 
-    @app.route("/mobile")
-    def mobile():
-        return render_template("mobile.html")
-    
-    # ----------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
+# Route per la pagina di login desktop
+@app.route("/login_desktop", methods=["GET"])
+def login_desktop():
+    return render_template("login_desktop.html")
 
-    def run_server():
-        logger.info(f"Avviando il sito sulla porta {port}...")
-        app.run(debug=True, host='0.0.0.0', port=port, use_reloader=False)
+# ----------------------------------------------------------------------------------------------------
+# Route per la pagina di login mobile
+@app.route("/login_mobile", methods=["GET"])
+def login_mobile():
+    return render_template("login_mobile.html")
 
-    run_server()
+# ----------------------------------------------------------------------------------------------------
+# Route per la pagina desktop
+@app.route("/desktop")
+def desktop():
+    return render_template("desktop.html")
+
+# ----------------------------------------------------------------------------------------------------
+# Route per la pagina mobile
+@app.route("/mobile")
+def mobile():
+    return render_template("mobile.html")
+
+# ----------------------------------------------------------------------------------------------------
+# Funzione per avviare il server
+def run_server(port):
+    logger.info(f"Avviando il sito sulla porta {port}...")
+    app.run(debug=True, host='0.0.0.0', port=port, use_reloader=False)
+
+# ----------------------------------------------------------------------------------------------------
+# Avvio del server sulla porta desiderata
+if __name__ == "__main__":
+    run_server(8080)
